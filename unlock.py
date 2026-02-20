1aecbed3f7e8ac2d657076a7d02d1a02d7f47b94
prahas10 <108119173+PrahasHegde@users.noreply.github.com>
2026-02-20 15:45:43 +0100

    Update unlock.py

--- unlock.py
+++ unlock.py
@@ -1,3 +1,8 @@
+# unlock.py -> code for the manual unlock function (admin feature) == main code
+#-----------------------------------------------------------------------------
+
+
+# IMPORTS
 import serial
 import time
 
@@ -8,14 +13,19 @@ try:
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     print("Connected on /dev/ttyACM0")
 except:
+    
     try:
+        
         # If that fails, try USB0
         ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
         print("Connected on /dev/ttyUSB0")
+     # Handle exception   
     except Exception as e:
         print(f"CRITICAL ERROR: Could not find Arduino! \n{e}")
         exit()
 
+
+#ACTION
 time.sleep(2) # Wait for Arduino to reset
 print("Sending Open Command...")
 ser.write(b'O')
